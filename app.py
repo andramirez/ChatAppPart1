fd0990211fd568d9f6fb24dc2534bdad0243ccbb
andramirez <andramirez@csumb.edu>
2017-02-17 20:57:42 +0000

    Update

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ all_msgs = []
 @socketio.on('new msg')
 def on_new_msg(data):
     if data['facebook_user_token'] == "":
-        response=requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=XYZ123' + data['google_user_token'])
+        response=requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + data['google_user_token'])
     else:
         response=requests.get('https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cpicture&access_token='+ data['facebook_user_token'])
     json=response.json()
