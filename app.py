217cfd4f00c781f61c611eb25eda36575f2c52b0
andramirez <andramirez@csumb.edu>
2017-02-17 22:28:48 +0000

    Update

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ all_msgs = []
 @socketio.on('new msg')
 def on_new_msg(data):
     if 'facebook_user_token' in data:
+        print 'I MADE IT INTO FACEBOOK';
         response = requests.get('https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cpicture&access_token='+ data['facebook_user_token'])
         json=response.json()
         all_msgs.append({
