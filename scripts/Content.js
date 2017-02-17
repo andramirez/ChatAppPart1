327b81f1d4c7f9f5b803321cbedc7983c98a6b01
andramirez <andramirez@csumb.edu>
2017-02-17 20:24:48 +0000

    Update

--- scripts/Content.js
+++ scripts/Content.js
@@ -37,14 +37,17 @@ export class Content extends React.Component {
       <div className="App" style={AppStyle}>
           <div id="live" className = "small-container">
             <h3>Please Sign In Using Google or Facebook</h3>
-            <div className="g-signin2" data-theme="dark"></div>
           <div 
           className="fb-login-button" 
           data-max-rows="1" 
           data-size="medium" 
           data-show-faces="false" 
           data-auto-logout-link="true">
-          </div>
+          </div>            
+          <div 
+            className="g-signin2" 
+            data-theme="dark">
+            </div>
         </div>
         <div id="msgBox">
           <h3>Welcome to the Chat Room</h3>
