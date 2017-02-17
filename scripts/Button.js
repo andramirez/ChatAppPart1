0d37b7c8c529b1941b1475a60b0d7538ec10c01f
andramirez <andramirez@csumb.edu>
2017-02-17 22:22:36 +0000

    Update

--- scripts/Button.js
+++ scripts/Button.js
@@ -15,12 +15,12 @@ export class Button extends React.Component {
                 });
             }
             else {
-                // Button.js, continued
                 let auth = gapi.auth2.getAuthInstance();
                 let user = auth.currentUser.get();
                 if(user.isSignedIn()){
+                    console.log(user.getAuthResponse().id_token)
                     Socket.emit('new msg',{
-                        'google_user_token':user.getAuthResponse().id_token,
+                        'google_user_token': user.getAuthResponse().id_token,
                         'msg': document.getElementById("msg").value,
                         
                     });
