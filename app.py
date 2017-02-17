cb48da37b425b09e3ecd674f2c63d13024f65e65
andramirez <andramirez@csumb.edu>
2017-02-17 21:27:30 +0000

    Update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import flask
 import flask_socketio
 import requests
-# from oauth2client import client, crypt
+from oauth2client import client, crypt
 
 app = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(app)
@@ -23,17 +23,7 @@ all_msgs = []
 @socketio.on('new msg')
 def on_new_msg(data):
     if data['facebook_user_token'] == "":
-        print 'd'
-        # try:
-        #     idinfo = client.verify_id_token(data['google_user_token'], '985898840926-hgsdmkcas5969msavv6oe3tf4e651k5d.apps.googleusercontent.com')
-        
-        #     if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-        #         raise crypt.AppIdentityError("Wrong issuer.")
-        # except crypt.AppIdentityError:
-        #     # Invalid token
-        #     print 'Invalid Token'
-        # userid = idinfo['sub']
-        # response = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + userid)
+        response = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + data['google_user_token'])
     else:
         response = requests.get('https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cpicture&access_token='+ data['facebook_user_token'])
     json=response.json()
