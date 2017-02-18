cf6f76eb55daf2981372fad1bcf13cbd2c53892a
andramirez <andramirez@csumb.edu>
2017-02-18 23:31:06 +0000

    Update

--- models.py
+++ models.py
@@ -7,14 +7,14 @@ db = flask_sqlalchemy.SQLAlchemy(app.app)
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True) # key
-    name = db.Column(db.String(120))
     pic = db.Column(db.String(300))
-    msg = db.Column(db.String(120))
+    name = db.Column(db.String(500))
+    msg = db.Column(db.String(500))
     
-    def __init__(self, content):
-        self.name = content['name']
-        self.pic = content['picture']
-        self.msg = content['msgs']
+    def __init__(self, p,n,m):
+        self.pic = p
+        self.name = n
+        self.msg = m
     
     def __repr__(self): 
         return '<Message text: %s %s %s>' % self.pic %self.name %self.msg
