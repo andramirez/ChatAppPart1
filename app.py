27b5ebf92459679f36795ff95229217fae2d6f65
andramirez <andramirez@csumb.edu>
2017-02-19 00:35:20 +0000

    Update

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def on_new_msg(data):
             'msgs':data['msg']
             })
             
-        models.db.session.add(models.Message(all_msgs['picture'], all_msgs['name'], all_msgs['msgs']))
+        models.db.session.add(models.Message(all_msgs['picture'], json['name'], all_msgs['msgs']))
         models.db.session.commit()
         
         socketio.emit('all msgs', {
