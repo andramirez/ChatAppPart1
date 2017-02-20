b9c908db4a5c567a794f2f4628cfce4023086c83
andramirez <andramirez@csumb.edu>
2017-02-20 02:42:20 +0000

    Fixing an indent

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import models
 
 ##current database print out
 @app.route('/')
-def index():
+# def index():
     # messages = models.Message.query.all()
     # html = ['<li>' + m.picture + m.name + m.message + '<li>' for m in messages]
     # return '<ul>' + ''.join(html) + '</ul>'
