a1b37daafe6415a6f7217936b141431bfb283ef3
andramirez <andramirez@csumb.edu>
2017-02-20 21:23:43 +0000

    Update

--- scripts/Content.js
+++ scripts/Content.js
@@ -39,10 +39,10 @@ export class Content extends React.Component {
             (n, index) => <div key = {index} style={style} id="text1"><img src={n.picture}/><b>{n.name}:</b>{n.msgs}</div>
     );
     
-    // //goes in users tag. List of users in the chat
-    // let users = this.state.users.map(
-    //         (u, user) => <div key = {user} style={style} id="users"><b>{u.name}</b></div>
-    // );
+    //goes in users tag. List of users in the chat
+    let users = this.state.users.map(
+            (u, user) => <li key = {user} style={style} id="users"><b>{u.name}</b></li>
+    );
     // //goes in users tag. Is the count of how many users is in the chat
     // let counts = this.state.users.map(
     //         (c, count) => <div key = {count} id="count"><b>{c.count}</b></div>
@@ -57,8 +57,10 @@ export class Content extends React.Component {
         <div id="msgBox">
           <div id="welcome">Welcome to the Chat Room</div>
           <div id="chatbox" style = {chat}>
-            <div id="users">Number of users: </div>
-            <div id="log">{msgs}</div> 
+            <div id="users">Number of users: <br/>
+            <ul>{users}</ul>
+            </div>
+            <div id="log" onload='componentDidMount()'>{msgs}</div> 
           </div>
         <Button/>
         </div>
