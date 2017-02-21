a71e8b01b56068f7a26b71ffc3a03ce46dfc2008
andramirez <andramirez@csumb.edu>
2017-02-21 16:11:24 +0000

    Database load

--- app.py
+++ app.py
@@ -95,37 +95,53 @@ def bot_msg(argument):
 @socketio.on('new msg')
 def on_new_msg(data):
     if 'facebook_user_token' in data:
-        response = requests.get('https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cpicture&access_token='+ data['facebook_user_token'])
-        json=response.json()
-        all_msgs.append({ ##retrieving facebook data
-            'name':" " + json['name'],
-            'picture':json['picture']['data']['url'],
-            'msgs':data['msg']
-            })
-        ##add data to the database    
-        models.db.session.add(models.Message(json['picture']['data']['url'], json['name'], data['msg']))
-        models.db.session.commit()
-        
-        socketio.emit('all msgs', {
-            'msgs': all_msgs
-        })
-         ##checks if bot was referenced with !!
-        if "!!" in data['msg']:
+        if "!! connected" in data['msg']:
+            response = requests.get('https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cpicture&access_token='+ data['facebook_user_token'])
+            json=response.json()
             bot = bot_msg(data['msg'])
-            if "connected" in data['msg']:
-                bot = bot + json['name']
+            bot = bot + json['name']
             all_msgs.append({
             'name':" bot.bot",
             'picture':"https://camo.githubusercontent.com/95cd3ddb1c8f475ae0893a711d470c1bd4fd67d1/687474703a2f2f696d616765732e736f6674776172652e636f6d2f6d61632e636f6d2e666c69706c6576656c2e63686174626f742f69636f6e2d3132382e706e67",
             'msgs':bot
             })
-            models.db.session.add(models.Message(u'https://camo.githubusercontent.com/95cd3ddb1c8f475ae0893a711d470c1bd4fd67d1/687474703a2f2f696d616765732e736f6674776172652e636f6d2f6d61632e636f6d2e666c69706c6576656c2e63686174626f742f69636f6e2d3132382e706e67', u' bot.bot', "u'"+bot+"'"))
+            models.db.session.add(models.Message(u'https://camo.githubusercontent.com/95cd3ddb1c8f475ae0893a711d470c1bd4fd67d1/687474703a2f2f696d616765732e736f6674776172652e636f6d2f6d61632e636f6d2e666c69706c6576656c2e63686174626f742f69636f6e2d3132382e706e67', 'bot.bot', bot))
             models.db.session.commit()
             
             socketio.emit('all msgs', {
                 'msgs': all_msgs
-        }) 
-        #Google auth not implemented.. 
+            }) 
+        else:
+            response = requests.get('https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cpicture&access_token='+ data['facebook_user_token'])
+            json=response.json()
+            all_msgs.append({ ##retrieving facebook data
+                'name':" " + json['name'],
+                'picture':json['picture']['data']['url'],
+                'msgs':data['msg']
+                })
+            ##add data to the database    
+            models.db.session.add(models.Message(json['picture']['data']['url'], json['name'], data['msg']))
+            models.db.session.commit()
+            
+            socketio.emit('all msgs', {
+                'msgs': all_msgs
+            })
+             ##checks if bot was referenced with !!
+            if "!!" in data['msg']:
+                bot = bot_msg(data['msg'])
+                bot = bot + json['name']
+                all_msgs.append({
+                'name':" bot.bot",
+                'picture':"https://camo.githubusercontent.com/95cd3ddb1c8f475ae0893a711d470c1bd4fd67d1/687474703a2f2f696d616765732e736f6674776172652e636f6d2f6d61632e636f6d2e666c69706c6576656c2e63686174626f742f69636f6e2d3132382e706e67",
+                'msgs':bot
+                })
+                models.db.session.add(models.Message(u'https://camo.githubusercontent.com/95cd3ddb1c8f475ae0893a711d470c1bd4fd67d1/687474703a2f2f696d616765732e736f6674776172652e636f6d2f6d61632e636f6d2e666c69706c6576656c2e63686174626f742f69636f6e2d3132382e706e67', 'bot.bot', bot))
+                models.db.session.commit()
+                
+                socketio.emit('all msgs', {
+                    'msgs': all_msgs
+            }) 
+            #Google auth not implemented.. 
     else:
         print 'I MADE IT INTO GOOGLE';
         response = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + data['google_user_token'])
