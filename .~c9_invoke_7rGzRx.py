65cea8cebe309f1c9244d03f7aa57e5077b7fbed
andramirez <andramirez@csumb.edu>
2017-02-21 15:06:02 +0000

    Socket work

---
+++ .~c9_invoke_7rGzRx.py
@@ -0,0 +1,21 @@
+import flask_sqlalchemy
+import app
+import os
+
+app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+db = flask_sqlalchemy.SQLAlchemy(app.app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # key
+    picture = db.Column(db.String(300))
+    name = db.Column(db.String(500))
+    message = db.Column(db.String(500))
+    
+    def __init__(self, p,n,m):
+        self.picture = p
+        self.name = n
+        self.message = m
+    
+    def __repr__(self): 
+        return '<Message text: %s %s %s>' % self.picture %self.name %self.message
